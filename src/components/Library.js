6fce54563f66192c5da4aabb51ed07fd35560c6d
Constantin <constantinu93@gmail.com>
2021-01-12 19:16:17 +0100

    feat: create and complete the Library component

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import BookShelf from './BookShelf';
 
-const bookShelfs = ["Currently Reading", "Want to Read", "Read"];
+
 
 const Library = props => {
-    const { books } = props;
+    const { books, selectShelf } = props;
+    /*     
+    create an object for the 3 different shelfes and there ID which can be matched with the "shelf"
+    property from the books from the BooksAPI: */
+    const bookShelfs = [
+        { id: "currentlyReading", name: "Currently Reading" },
+        { id: "wantToRead", name: "Want to Read" },
+        { id: "read", name: "Read" }
+    ];
 
-    return(
+    /* 
+    first: map over the bookShelfs object
+    second: create the booksOnTheShelf constant to to filter only the books that belong to the particular shelf.
+    third: return a BookShelf Component for each of the three bookShelfs from the bookShelfs object.
+     */
+    return (
         <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-                {bookShelfs.map((bookShelf, index) => (
-                    <BookShelf
-                        key={bookShelf}
-                        title={bookShelf}
-                        books={books}
-                    />
-                ))}
-                {/* <div className="open-search">
-                    <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-                </div> */}
+                {bookShelfs.map((bookShelf) => {
+                    const booksOnTheShelf = books.filter(book => book.shelf === bookShelf.id);
+                    return(
+                        <div className="bookshelf" key={bookShelf}>
+                            <h2 className="bookshelf-title">{bookShelf.name}</h2>
+                            <div className="bookshelf-books">
+                                <BookShelf
+                                    books={booksOnTheShelf}
+                                    selectShelf={selectShelf}
+                                />
+                            </div>
+                        </div>
+                    );
+                })
+                }
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Library;

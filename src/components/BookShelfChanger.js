5fa6e81db6252ee18c6a0d3e15dbd82f8e348188
Constantin Unterkofler <constantinu93@gmail.com>
2021-01-15 20:44:06 +0100

    feat: add default shelf option

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -2,19 +2,30 @@ import React from 'react'
 
 class BookShelfChanger extends React.Component {
     
-
     handleChange = event => {
         const { value } = event.target
         this.props.onSelectShelf(this.props.book, value);
     }
 
-
     render() {
-        const { book } = this.props;
+        const { book, books } = this.props;
+
+        /* The option "None" is selected as default, if a book has not been assigned to a shelf: */
+        let defaultShelf = "none";
+
+        /*         
+        If a book is assigned to a shelf on the main page and that book appears on
+        the search page, the correct shelf is be selected on the search page:
+        */
+        books.forEach((element) => {
+            if (element.title === book.title) {
+                defaultShelf = element.shelf
+            }
+        })
 
         return (
             <div className="book-shelf-changer">
-                <select value={book.shelf} onChange={this.handleChange}>
+                <select value={defaultShelf} onChange={this.handleChange}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>

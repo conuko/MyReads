29ae1e80df2bc25b07f1557c8795b0f3112e188e
Constantin Unterkofler <constantinu93@gmail.com>
2021-01-19 14:51:11 +0100

    refactor: refactoring the BookShelf production code

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,24 +2,34 @@ import React from 'react';
 import Book from './Book';
 
 const BookShelf = props => {
-    const { books, onSelectShelf } = props;
+    const { books, bookShelf, onSelectShelf } = props;
+    /*
+    create the booksOnTheShelf constant to filter 
+    only the books that belong to a particular shelf:
+    */
+    const booksOnTheShelf = books.filter((book) => book.shelf === bookShelf.id);
 
     /* 
     Map over the "books" props to render every single book with the Book Component
     and pass the Book Component "books", "book" and "selectShelf" as Props
     */
     return (
-          <ol className="books-grid">
-            {books.map(book => {
-                return (
-                    <Book key={book.id}
-                        books={books}
-                        book={book}
-                        onSelectShelf={onSelectShelf}
-                    />
-                )
-            })}
-          </ol>
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{bookShelf.name}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {booksOnTheShelf.map(book => {
+                        return (
+                            <Book key={book.id}
+                                books={books}
+                                book={book}
+                                onSelectShelf={onSelectShelf}
+                            />
+                        )
+                    })}
+                </ol>
+            </div>
+        </div>
     );
 };
 

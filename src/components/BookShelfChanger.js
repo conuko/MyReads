b919113da045be90ba47fcbae17efac931e93418
Constantin <constantinu93@gmail.com>
2021-01-21 12:56:53 +0100

    fix: books in search page not showing exact shelves

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -13,12 +13,33 @@ class BookShelfChanger extends React.Component {
         this.props.onSelectShelf(this.props.book, value);
     };
 
+    
+
     render() {
-        const { book } = this.props;
+        const { book, books } = this.props;
+
+        /*
+        When a book that is already on the bookshelf appears on the search page, it should have the same state.
+        E.g if it is set to the 'Currently Reading' shelf then it should also have that option
+        on the search page. Otherwise it should be set to 'None':
+        */
+        let currentBookShelf;
+        books.forEach((item) => {
+            // check, if a book (item) is on a shelf (book --> "book" are the books that are on a shelf)
+            if (item.id === book.id) {
+                /*
+                if the checked book is already on a shelf, the status of the currentBookShelf of the book
+                is set to the the name of the shelf, e.g 'currently reading':
+                */
+                currentBookShelf = item.shelf;
+            }
+        })
+
+
 
         return (
             <div className="book-shelf-changer">
-                <select value={book.shelf ? book.shelf : "none"} onChange={this.handleChange}>
+                <select value={currentBookShelf ? currentBookShelf : "none"} onChange={this.handleChange}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>

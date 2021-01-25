acbf0763f31aa69b77896db082b11f0955cf2225
Constantin <constantinu93@gmail.com>
2021-01-25 17:42:45 +0100

    fix: minor bug fix with multiple authors

--- src/components/Book.js
+++ src/components/Book.js
@@ -20,7 +20,7 @@ const Book = props => {
                     />
                 </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors.join(', ')}</div>
+            <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
             </div>
         </li>
     )

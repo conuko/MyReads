3ff3d4cd33cc9aaba8ee1d105d70b597b30d2022
Constantin <constantinu93@gmail.com>
2021-01-25 18:07:00 +0100

    feat: convert BookShelfChanger to functional component

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-class BookShelfChanger extends React.Component {
+const BookShelfChanger = (props) => {
+    const { book, books } = props;
     
-    handleChange = event => {
+    const handleChange = event => {
         const { value } = event.target
         /*
         Call the callback function that was passed to this component from App.js 
@@ -13,38 +14,36 @@ class BookShelfChanger extends React.Component {
         this.props.onSelectShelf(this.props.book, value);
     };
 
-    render() {
-        const { book, books } = this.props;
+        
 
-        /*
-        When a book that is already on the bookshelf appears on the search page, it should have the same state.
-        E.g if it is set to the 'Currently Reading' shelf then it should also have that option
-        on the search page. Otherwise it should be set to 'None':
-        */
-        let currentBookShelf;
-        books.forEach((item) => {
-            // check, if a book (item) is on a shelf (book --> "book" are the books that are on a shelf)
-            if (item.id === book.id) {
-                /*
-                if the checked book is already on a shelf, the status of the currentBookShelf of the book
-                is set to the the name of the shelf, e.g 'currently reading':
-                */
-                currentBookShelf = item.shelf;
-            }
-        })
+    /*
+    When a book that is already on the bookshelf appears on the search page, it should have the same state.
+    E.g if it is set to the 'Currently Reading' shelf then it should also have that option
+    on the search page. Otherwise it should be set to 'None':
+    */
+    let currentBookShelf;
+    books.forEach((item) => {
+        // check, if a book (item) is on a shelf (book --> "book" are the books that are on a shelf)
+        if (item.id === book.id) {
+            /*
+            if the checked book is already on a shelf, the status of the currentBookShelf of the book
+            is set to the the name of the shelf, e.g 'currently reading':
+            */
+            currentBookShelf = item.shelf;
+        };
+    });
 
-        return (
-            <div className="book-shelf-changer">
-                <select value={currentBookShelf ? currentBookShelf : "none"} onChange={this.handleChange}>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-            </div>
-        );
-    };
+    return (
+        <div className="book-shelf-changer">
+            <select value={currentBookShelf ? currentBookShelf : "none"} onChange={handleChange}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+    );
 };
 
 export default BookShelfChanger;

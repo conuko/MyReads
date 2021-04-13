35d6b170f29c398b2ed39554e92a1c35d537f8d1
Constantin <constantinu93@gmail.com>
2021-04-13 16:12:24 +0200

    feat: change Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>, document.getElementById('root'));
+    </BrowserRouter>, document.getElementById('root'));
